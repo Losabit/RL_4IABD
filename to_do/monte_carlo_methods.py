4188d1e30eba84e37286e012ff39e6f2e50acf5a
Paul <paulbenabou@gmail.com>
2021-07-07 16:42:52 +0200

    Progression askip

--- to_do/monte_carlo_methods.py
+++ to_do/monte_carlo_methods.py
@@ -6,7 +6,16 @@ import random
 import numpy as np
 
 
-def tic_tac_toe_env():
+def get_best_tic_tac_toe_play(available_actions, q, S, round_counter):
+    for i in range(len(list(q[S[round_counter]].keys()))-1, 0, -1):
+        best_action_value = np.sort(list(q[S[round_counter]].values()))[i] #trie
+
+        best_action = list(q[S[round_counter]].keys())[list(q[S[round_counter]].values()).index(best_action_value)]
+        if best_action in available_actions:
+            return best_action
+
+
+def tic_tac_toe_env(pi, q):
     env = TicTacToe()
     X = 600
     Y = 600
@@ -16,8 +25,13 @@ def tic_tac_toe_env():
     background = pygame.image.load('assets/tictactoe/background.png')
     x = pygame.image.load('assets/tictactoe/x.png')
     o = pygame.image.load('assets/tictactoe/o.png')
-    turn = 0
+    turn = 1
     game_finished = False
+
+    if pi and q:
+        monte_carlo_playing = True
+        S = []
+        round_counter = 0
 
     while True:
         screen.fill((255, 255, 255))
@@ -35,10 +49,21 @@ def tic_tac_toe_env():
                 pygame.quit()
                 quit()
             elif event.type == pygame.MOUSEBUTTONUP and turn == 0 and not game_finished:
-                pos = pygame.mouse.get_pos()
-                column = int(pos[0] // (X / 3))
-                line = int(pos[1] // (Y / 3))
-                env.act_with_action_id(column + line * 3)
+                if monte_carlo_playing:
+                    s = env.state_id()
+                    S.append(s)
+                    available_actions = env.available_actions_ids()
+
+                    best_action = get_best_tic_tac_toe_play(available_actions, q, S, round_counter)
+
+                    env.act_with_action_id(best_action)
+                    round_counter = round_counter + 1
+                else:
+                    pos = pygame.mouse.get_pos()
+                    column = int(pos[0] // (X / 3))
+                    line = int(pos[1] // (Y / 3))
+
+                    env.act_with_action_id(column + line * 3)
                 turn = 1
                 if env.is_game_over() and not game_finished:
                     game_finished = True
@@ -61,7 +86,64 @@ def monte_carlo_es_on_tic_tac_toe_solo() -> PolicyAndActionValueFunction:
     Launches a Monte Carlo ES (Exploring Starts) in order to find the optimal Policy and its action-value function
     Returns the Optimal Policy (Pi(s,a)) and its Action-Value function (Q(s,a))
     """
-    pass
+    env = TicTacToe()
+    max_episodes_count = 1000
+    gamma = 0.9
+
+    pi = {}
+    q = {}
+    returns = {}
+
+    for ep in range(max_episodes_count):
+        env.reset()
+
+        S = []
+        A = []
+        R = []
+
+        G = 0
+        while not env.is_game_over():
+            s = env.state_id()
+            S.append(s)
+            available_actions = env.available_actions_ids()
+
+            if s not in pi:
+                pi[s] = {}
+                q[s] = {}
+                returns[s] = {}
+                for a in available_actions:
+                    pi[s][a] = 1.0 / len(available_actions)
+                    q[s][a] = 0.0
+                    returns[s][a] = []
+
+            chosen_action = available_actions[np.random.randint(len(available_actions))]
+            A.append(chosen_action)
+
+            old_score = env.score()
+            env.act_with_action_id(chosen_action)
+            r = env.score() - old_score
+            R.append(r)
+
+            for t in reversed(range(len(S))):
+                G = gamma * G + R[t]
+
+                found = False
+                for prev_s, prev_a in zip(S[:t], A[:t]):
+                    if prev_s == S[t] and prev_a == A[t]:
+                        found = True
+                        break
+                if found:
+                    continue
+
+                if A[t] not in returns[S[t]]:
+                    returns[S[t]][A[t]] = []
+
+                returns[S[t]][A[t]].append(G)
+                q[S[t]][A[t]] = np.mean(returns[S[t]][A[t]])
+
+                for a_key in pi[S[t]].keys():
+                    pi[S[t]][a_key] = np.argmax(q[S[t]][a_key])
+    return pi, q
 
 
 def on_policy_first_visit_monte_carlo_control_on_tic_tac_toe_solo() -> PolicyAndActionValueFunction:
@@ -189,11 +271,12 @@ def off_policy_monte_carlo_control_on_secret_env2() -> PolicyAndActionValueFunct
 
 
 def demo():
-    #tic_tac_toe_env()
-    print(monte_carlo_es_on_tic_tac_toe_solo())
-    print(on_policy_first_visit_monte_carlo_control_on_tic_tac_toe_solo())
-    print(off_policy_monte_carlo_control_on_tic_tac_toe_solo())
+    pi, q = monte_carlo_es_on_tic_tac_toe_solo()
+    tic_tac_toe_env(pi, q)
 
-    print(monte_carlo_es_on_secret_env2())
-    print(on_policy_first_visit_monte_carlo_control_on_secret_env2())
-    print(off_policy_monte_carlo_control_on_secret_env2())
+    # print(on_policy_first_visit_monte_carlo_control_on_tic_tac_toe_solo())
+    # print(off_policy_monte_carlo_control_on_tic_tac_toe_solo())
+
+# print(monte_carlo_es_on_secret_env2())
+# print(on_policy_first_visit_monte_carlo_control_on_secret_env2())
+# print(off_policy_monte_carlo_control_on_secret_env2())
