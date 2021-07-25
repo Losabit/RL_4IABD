dc7f6e2397c8564d540b73a09e347643287aaad1
Quentin <quentin.clodion@gmail.com>
2021-07-25 15:57:27 +0200

    qlearning

--- to_do/dynamic_programming.py
+++ to_do/dynamic_programming.py
@@ -317,8 +317,56 @@ def policy_iteration_on_secret_env1() -> PolicyAndValueFunction:
     Returns the Policy (Pi(s,a)) and its Value Function (V(s))
     """
     env = Env1()
-    # TODO
-    pass
+    V = np.zeros((len(env.states()),))
+    pi = np.ones((len(env.states()), len(env.actions())))
+    pi /= len(env.actions())
+
+    theta = 0.00001
+    gamma = 1.0
+
+    while True:
+        while True:
+            delta = 0
+            for s in env.states():
+                old_v = V[s]
+                V[s] = 0.0
+                for a in env.actions():
+                    for s_next in env.states():
+                        for r_idx, r in enumerate(env.rewards()):
+                            V[s] += pi[s, a] * env.transition_probability(s, a, s_next, r_idx) * (r + gamma * V[s_next])
+                delta = max(delta, abs(V[s] - old_v))
+
+            if delta < theta:
+                break
+
+        policy_stable = True
+        for s in env.states():
+            old_policy = pi[s, :]
+
+            best_a = None
+            best_a_value = None
+            for a in env.actions():
+                a_value = 0
+                for s_p in env.states():
+                    for r_idx, r in enumerate(env.rewards()):
+                        a_value += env.transition_probability(s, a, s_p, r_idx) * (r + gamma * V[s_p])
+                if best_a_value is None or best_a_value < a_value:
+                    best_a_value = a_value
+                    best_a = a
+
+            pi[s, :] = 0.0
+            pi[s, best_a] = 1.0
+            if not np.array_equal(pi[s], old_policy):
+                policy_stable = False
+
+        if policy_stable:
+            break
+
+    final_pi = {}
+    for indice, value in enumerate(pi):
+        final_pi[indice] = dict(enumerate(value.flatten(), 1))
+
+    return PolicyAndValueFunction(final_pi, dict(enumerate(V.flatten(), 1)))
 
 
 def value_iteration_on_secret_env1() -> PolicyAndValueFunction:
@@ -328,8 +376,44 @@ def value_iteration_on_secret_env1() -> PolicyAndValueFunction:
     Prints the Policy (Pi(s,a)) and its Value Function (V(s))
     """
     env = Env1()
-    # TODO
-    pass
+    V = np.zeros((len(env.states()),))
+    pi = np.ones((len(env.states()), len(env.actions())))
+    pi /= len(env.actions())
+    pi2 = pi.copy()
+
+    theta = 0.00001
+    gamma = 1.0
+
+    while True:
+        delta = 0
+        for s in env.states():
+            old_v = V[s]
+            V[s] = 0.0
+            best_a_value = None
+            best_a = None
+            for a in env.actions():
+                a_value = 0
+                for s_p in env.states():
+                    for r_idx, r in enumerate(env.rewards()):
+                        pre_a_value = env.transition_probability(s, a, s_p, r_idx) * (r + gamma * V[s_p])
+                        a_value += pre_a_value
+                        V[s] += pi[s, a] * pre_a_value
+                if best_a_value is None or best_a_value < a_value:
+                    best_a_value = a_value
+                    best_a = a
+
+            delta = max(delta, abs(V[s] - old_v))
+            pi2[s, :] = 0.0
+            pi2[s, best_a] = 1.0
+
+        if delta < theta:
+            break
+
+    final_pi = {}
+    for indice, value in enumerate(pi2):
+        final_pi[indice] = dict(enumerate(value.flatten(), 1))
+
+    return PolicyAndValueFunction(final_pi, dict(enumerate(V.flatten(), 1)))
 
 
 def demo():
--- to_do/temporal_difference_learning.py
+++ to_do/temporal_difference_learning.py
@@ -1,5 +1,83 @@
+from tqdm import tqdm
 from do_not_touch.result_structures import PolicyAndActionValueFunction
 from do_not_touch.single_agent_env_wrapper import Env3
+from envs.TicTacToe import TicTacToe, tic_tac_toe_env
+import numpy as np
+
+
+def algo_q_learning(env) -> PolicyAndActionValueFunction:
+    alpha = 0.1
+    epsilon = 1.0
+    gamma = 0.9
+    max_iter = 10000
+
+    pi = {}  # learned greedy policy
+    b = {}  # behaviour epsilon-greedy policy
+    q = {}  # action-value function of pi
+
+    for it in tqdm(range(max_iter)):
+        env.reset()
+
+        while not env.is_game_over():
+            s = env.state_id()
+            available_actions = env.available_actions_ids()
+            if s not in pi:
+                pi[s] = {}
+                q[s] = {}
+                b[s] = {}
+                for a in available_actions:
+                    pi[s][a] = 1.0 / len(available_actions)
+                    q[s][a] = 0.0
+                    b[s][a] = 1.0 / len(available_actions)
+            print(list(b[s].keys()))
+            print(list(b[s].values()))
+            print(available_actions)
+            # actions disponibles differents selon les states
+            available_actions_count = len(available_actions)
+            optimal_a = list(q[s].keys())[np.argmax(list(q[s].values()))]
+            for a_key, q_s_a in q[s].items():
+                if a_key == optimal_a:
+                    b[s][a_key] = 1 - epsilon + epsilon / available_actions_count
+                elif a_key not in available_actions:
+                    b[s][a_key] = 0
+                else:
+                    b[s][a_key] = epsilon / available_actions_count
+
+            print(list(b[s].values()))
+            chosen_action = np.random.choice(
+                list(b[s].keys()),
+                1,
+                False,
+                p=list(b[s].values())
+            )[0]
+            old_score = env.score()
+            env.act_with_action_id(chosen_action)
+            r = env.score() - old_score
+            s_p = env.state_id()
+            next_available_actions = env.available_actions_ids()
+
+            if env.is_game_over():
+                q[s][chosen_action] += alpha * (r + 0.0 - q[s][chosen_action])
+            else:
+                if s_p not in pi:
+                    pi[s_p] = {}
+                    q[s_p] = {}
+                    b[s_p] = {}
+                    for a in next_available_actions:
+                        pi[s_p][a] = 1.0 / len(next_available_actions)
+                        q[s_p][a] = 0.0
+                        b[s_p][a] = 1.0 / len(next_available_actions)
+                q[s][chosen_action] += alpha * (r + gamma * np.max(list(q[s_p].values())) - q[s][chosen_action])
+
+    for s in q.keys():
+        optimal_a = list(q[s].keys())[np.argmax(list(q[s].values()))]
+        for a_key, q_s_a in q[s].items():
+            if a_key == optimal_a:
+                pi[s][a_key] = 1.0
+            else:
+                pi[s][a_key] = 0.0
+
+    return PolicyAndActionValueFunction(pi, q)
 
 
 def sarsa_on_tic_tac_toe_solo() -> PolicyAndActionValueFunction:
@@ -20,8 +98,8 @@ def q_learning_on_tic_tac_toe_solo() -> PolicyAndActionValueFunction:
     Returns the optimal greedy Policy and its Action-Value function (Q(s,a))
     Experiment with different values of hyper parameters and choose the most appropriate combination
     """
-    # TODO
-    pass
+    env = TicTacToe()
+    return algo_q_learning(env)
 
 
 def expected_sarsa_on_tic_tac_toe_solo() -> PolicyAndActionValueFunction:
@@ -73,7 +151,8 @@ def expected_sarsa_on_secret_env3() -> PolicyAndActionValueFunction:
 
 def demo():
     print(sarsa_on_tic_tac_toe_solo())
-    print(q_learning_on_tic_tac_toe_solo())
+    trained = q_learning_on_tic_tac_toe_solo()
+    tic_tac_toe_env(trained.pi, trained.q)
     print(expected_sarsa_on_tic_tac_toe_solo())
 
     print(sarsa_on_secret_env3())
